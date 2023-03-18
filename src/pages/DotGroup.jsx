5ca99c5c5173362eaccde340f743229fdd14b152
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2023-03-18 20:44:00 -0300

    fix selectedStyles before

--- src/pages/DotGroup.jsx
+++ src/pages/DotGroup.jsx
@@ -1,8 +1,8 @@
 import AnchorLink from "react-anchor-link-smooth-scroll-v2";
 function DotGroup({ selectedPage, setSelectedPage }) {
-  const selectedStyles = `relative bg-yellow-300 before:absolute before:w-6 before:h-6
-  before:rounded-full before:boder-2 before:border-yellow-800 before:left-[-50%]
-   before:top-[-50%]
+  const selectedStyles = `relative bg-white before:absolute before:w-2 before:h-2
+  before:rounded-full before:boder-2 before:border-darkSecondary before:left-[50%] before:-translate-x-1/2 before:-translate-y-1/2 before:bg-darkSecondary
+   before:top-[50%]
   `;
 
   return (
@@ -10,35 +10,35 @@ function DotGroup({ selectedPage, setSelectedPage }) {
       <AnchorLink
         className={`${
           selectedPage === "home" ? selectedStyles : "dark:bg-white"
-        } h-3 w-3 rounded-full `}
+        } h-4 w-4 rounded-full `}
         href="#home"
         onClick={() => setSelectedPage("home")}
       />
       <AnchorLink
         className={`${
           selectedPage === "Conoceme" ? selectedStyles : "dark:bg-white"
-        } h-3 w-3 rounded-full `}
+        } h-4 w-4 rounded-full `}
         href={`#Conoceme`}
         onClick={() => setSelectedPage("Conoceme")}
       />
       <AnchorLink
         className={`${
           selectedPage === "Habilidades" ? selectedStyles : "dark:bg-white"
-        } h-3 w-3 rounded-full `}
+        } h-4 w-4 rounded-full `}
         href={`#Habilidades`}
         onClick={() => setSelectedPage("Habilidades")}
       />
       <AnchorLink
         className={`${
           selectedPage === "Proyectos" ? selectedStyles : "dark:bg-white"
-        } h-3 w-3 rounded-full `}
+        } h-4 w-4 rounded-full `}
         href={`#Proyectos`}
         onClick={() => setSelectedPage("Proyectos")}
       />
       <AnchorLink
         className={`${
           selectedPage === "Contacto" ? selectedStyles : "dark:bg-white"
-        } h-3 w-3 rounded-full `}
+        } h-4 w-4 rounded-full `}
         href={`#Contacto`}
         onClick={() => setSelectedPage("Contacto")}
       />
