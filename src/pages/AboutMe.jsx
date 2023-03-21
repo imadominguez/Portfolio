e7327ff3967067a9fafd0e92ab2e389f8c17b4f0
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2023-03-21 18:27:43 -0300

    about me finalizado

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -3,29 +3,84 @@ import useMediaQuery from "../hooks/useMediaQuery";
 import { motion } from "framer-motion";
 function AboutMe() {
   const isAboveMediumScreens = useMediaQuery("min-width: 920px");
+
+  const variants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: { duration: 1.5 } },
+  };
+  const variantH2 = {
+    hidden: { x: -50 },
+    visible: {
+      x: 10,
+      transition: { duration: 1.2, type: "spring" },
+    },
+  };
+  const variantSpanOne = {
+    hidden: { x: 50 },
+    visible: { x: 0, transition: { duration: 1, type: "spring" } },
+  };
+  const variantSpanTwo = {
+    hidden: { x: -50 },
+    visible: { x: 0, transition: { duration: 1, type: "spring" } },
+  };
   return (
-    <section id="Conoceme" className="mx-auto h-screen w-4/5  py-24">
-      <h2 className="font-robotoMono uppercase tracking-[1px] md:px-36 ">
+    <section
+      id="aboutme"
+      className=" mx-auto flex min-h-screen w-4/5 max-w-3xl flex-col items-center justify-center py-24 md:pt-48"
+    >
+      <motion.h2
+        variants={variantH2}
+        initial="hidden"
+        whileInView="visible"
+        className="font-robotoMono uppercase tracking-[1px]  "
+      >
         sobre mi
-      </h2>
-      <div className="mt-2 h-0.5 w-36 dark:bg-white md:mx-36"></div>
-      <div className="py-10">
+      </motion.h2>
+      <div className="mt-2 h-0.5 w-36 dark:bg-white "></div>
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        variants={variants}
+        className="py-10"
+      >
         <p className="flex flex-col font-robotoSerif">
           Hola👋.
-          <span className="pt-2 leading-6">
+          <motion.span
+            variants={variantSpanOne}
+            initial="hidden"
+            whileInView="visible"
+            className="pt-2 leading-6"
+          >
             Mi nombre es Imanol, soy de Buenos Aires, Argentina. Soy programador
             Full Stack y tutor e-learning con un año de experiencia en la
             industria de la educación. Me apasiona crear soluciones atractivas e
             innovadoras en el desarrollo de aplicaciones web, utilizando mis
             habilidades en HTML, CSS, JavaScript y React.
-          </span>
-          <span className="pt-2 leading-6">
+          </motion.span>
+          <motion.span
+            variants={variantSpanTwo}
+            initial="hidden"
+            whileInView="visible"
+            className="pt-2 leading-6"
+          >
             ¡Me encanta trabajar en equipo y siempre estoy buscando aprender
-            nuevas habilidades y conocimientos para poder brindar mis
-            conocimientos actualizados!💪
+            nuevas habilidades y conocimientos para poder estar siempre
+            actualizado!💪
+          </motion.span>
+          <motion.span
+            variants={variantSpanOne}
+            initial="hidden"
+            whileInView="visible"
+            className="mt-10 flex flex-col pt-2 text-center text-xl uppercase leading-6 text-darkSecondary"
+          >
+            Si te interesa un programador con ganas de progresar y afrontar
+            nuevos desafíos
+          </motion.span>
+          <span className="mt-4 text-center text-lg font-bold uppercase tracking-[1px] text-darkSecondary">
+            ¡Contacta conmigo!
           </span>
         </p>
-      </div>
+      </motion.div>
     </section>
   );
 }
